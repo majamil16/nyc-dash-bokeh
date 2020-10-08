8ee77aefed132adb1b0a323f9bb9d59e516014af
majamil16 <majam1616@gmail.com>
2020-10-08 20:49:36 +0000

    working!

--- nyc_dash/main.py
+++ nyc_dash/main.py
@@ -10,9 +10,12 @@ FILE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(FILE_DIR, 'data', '2020_closed_nyc_311.csv')
 #build the dataframe ONCE so that we can query it many times for updates.
 data, zips = da.construct_df(DATA_PATH)
+
+#might need to get rid of this line
+data.index.set_levels(['jan', 'feb', 'mar', 'apr','may', 'jun' ,'jul','aug','sep'],level=0,inplace=True)
+
 x = list(range(9)) # ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug","sep"] # x axis months
 # the y-values will depend on the zip code you choose
-#print(x)
 menu = list(zips)
 
 # the y-values of all the year data
@@ -26,10 +29,10 @@ drop1 = Dropdown(label="Zipcode 1", button_type="danger", menu=menu)
 drop2 = Dropdown(label="Zipcode 2", button_type="danger", menu=menu)
 
 def handler(event):
-    print(event.item)
-
-clicked_zip1 = drop1.on_click(handler)
-clicked_zip2 = drop2.on_click(handler)
+    #print(event.item)
+    update_plot(event.item)
+#clicked_zip1 = drop1.on_click(handler)
+#clicked_zip2 = drop2.on_click(handler)
 
 #zip1 = list(da.query_zip(data, clicked_zip1).values.flatten())
 #zip2 = list(da.query_zip(data, clicked_zip2).values.flatten())
@@ -51,31 +54,41 @@ colsource = {'x_vals':x,
 
 source = ColumnDataSource(colsource)
 
+source_fromdf = ColumnDataSource(data)
 
 
 #def update_plot():
-#    source.data.update('selected_zip1': clicked_zip1, "selected_zip2": clicked_zip2)
-
+ #   source.data.update('selected_zip1': clicked_zip1, "selected_zip2": clicked_zip2)
+    
 #clicked1.onclick(update plot)
 
 #f.multi_line([x,x,x],[plot_year, zip1, zip2])
-f.line('x_vals', 'yearly', source=source)
-f.line('x_vals', 'selected_zip1', source=source)
-f.line('x_vals', 'selected_zip2', source=source)
+year_line = f.line('x_vals', 'yearly', source=source, name='year_line')
+z1_line = f.line('x_vals', 'selected_zip1', source=source, name='z1_line')
+z2_line = f.line('x_vals', 'selected_zip2', source=source, name='z2_line')
+
+#update the plot with newly clicked data from the dropdown
+def update_plot(new):
+  #  clicked_zip1 = drop1.on_click(handler) 
+  #  clicked_zip2 = drop2.on_click(handler)
+    new_zip_vals = list(da.query_zip(data, zip_code=new ).values.flatten())
+    source.data.update({"selected_zip1":new_zip_vals}) # update the thing w the new value
+    
+    z1_line = f.select_one({'name':'z1_line'})
+  #  z2_line = f.select_one({'name':'z2_line'})
+    z1_line.data_source.data = dict(source.data)
+  #  z2_line.data_source.data = source.data
+    print("HERE")
+
+def update_plot2(new):
+    source.data.update({'selected_zip2':new}) # update the selected zip2 w the new value
+    z2_line = f.select_one({'name':'z2_line'})
+    z2_line.data_source.data = source.data
+
+#when either drop1 or drop2 are clicked, update the plot
+drop1.on_click(handler)
+#drop1.on_click(update_plot)
+#drop2.on_click("value", update_plot2)
+
 curdoc().add_root(column(drop1, drop2, f))
 
-"""
-x = list(range(11))
-y0 = x
-
-# create three plots
-s1 = figure(plot_width=250, plot_height=250, background_fill_color="#fafafa")
-s1.circle(x, y0, size=12, color="#53777a", alpha=0.8)
-
-menu=[("ZIP1", "zip1"), ("ZIP2", "zip2")]
-dropdown1=Dropdown(label="Zipcode 1", button_type="danger", menu=menu)
-dropdown2 = Dropdown(label="Zipcode 2", button_type="danger", menu=menu)
-
-
-curdoc().add_root(column(dropdown1, dropdown2, s1))
-"""
